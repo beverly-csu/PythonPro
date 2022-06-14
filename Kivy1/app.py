5702dc8229e26c77d91b95917b734ccef29a7dfd
beverly-csu <80579574+beverly-csu@users.noreply.github.com>
2022-06-14 20:10:11 +0500

    Update app.py

--- Kivy1/app.py
+++ Kivy1/app.py
@@ -1,26 +1,32 @@
-from kivy.app import App                                    # подключаем класс приложения
-from kivy.uix.label import Label                            # подключаем виджет "Надпись"
-from kivy.uix.button import Button                          # подключаем виджет "Кнопка"
-from kivy.uix.boxlayout import BoxLayout                    # подключаем виджет расположения объектов
-from kivy.uix.screenmanager import ScreenManager, Screen    # подключаем виджет "Экран"
+from kivy.app import App                                                        # подключаем класс приложения
+from kivy.uix.label import Label                                                # подключаем виджет "Надпись"
+from kivy.uix.button import Button                                              # подключаем виджет "Кнопка"
+from kivy.uix.boxlayout import BoxLayout                                        # подключаем виджет расположения объектов
+from kivy.uix.screenmanager import ScreenManager, Screen                        # подключаем виджет "Экран"
 
 
-class ScrButton(Button):                                    # создаем класс-наследник класса Button
-    def __init__(self, screen, direction='right', goal=''):
-        super().__init__()
+class ScrButton(Button):                                                        # создаем класс-наследник класса Button
+    def __init__(self, screen, direction='right', goal='main', **kwargs):       # описываем собственный конструктор
+        super().__init__(**kwargs)                                              # вызываем конструктор родительского класса
+        self.screen = screen                                                    # поле с объектом класса ScreenManager
+        self.direction = direction                                              # поле, описывающее вид перехода от окна к окну
+        self.goal = goal                                                        # поле, хранящее название окна, к которому будет совершен переход
+    def on_press(self):
+        self.screen.manager.transition.direction = self.direction
+        self.screen.manager.current = self.goal
 
 
-class MainScreen(Screen):                                   # создаем класс-наследник класса Screen
-    def __init__(self, **kwargs):                           # описываем собственный конструктор
-        super().__init__(**kwargs)                          # вызываем конструктор родительского класса
+class MainScreen(Screen):                                                       # создаем класс-наследник класса Screen
+    def __init__(self, **kwargs):                                               # описываем собственный конструктор
+        super().__init__(**kwargs)                                              # вызываем конструктор родительского класса
         v_layout = BoxLayout(orientation='vertical', padding=8, spacing=8)
         h_layout = BoxLayout()
         txt = Label(text='Выбери экран')
 
-class MyApp(App):                                           # создаем класс-наследник класса App
-    def build(self):                                        # переопределяем метод build
-        sm = ScreenManager()                                # создаем экземпляр класса ScreenManager()
-        return sm                                           # возвращаем наш экземпляр
+class MyApp(App):                                                               # создаем класс-наследник класса App
+    def build(self):                                                            # переопределяем метод build
+        sm = ScreenManager()                                                    # создаем экземпляр класса ScreenManager()
+        return sm                                                               # возвращаем наш экземпляр
 
 
 MyApp().run()
