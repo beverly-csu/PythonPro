dd5cfe2163c48256969960ba149ed559b2be16f2
beverly-csu <80579574+beverly-csu@users.noreply.github.com>
2022-06-14 20:06:14 +0500

    Update app.py

--- Kivy1/app.py
+++ Kivy1/app.py
@@ -5,9 +5,17 @@ from kivy.uix.boxlayout import BoxLayout                    # подключае
 from kivy.uix.screenmanager import ScreenManager, Screen    # подключаем виджет "Экран"
 
 
-class MainScreen(Screen):
-    pass
+class ScrButton(Button):                                    # создаем класс-наследник класса Button
+    def __init__(self, screen, direction='right', goal=''):
+        super().__init__()
 
+
+class MainScreen(Screen):                                   # создаем класс-наследник класса Screen
+    def __init__(self, **kwargs):                           # описываем собственный конструктор
+        super().__init__(**kwargs)                          # вызываем конструктор родительского класса
+        v_layout = BoxLayout(orientation='vertical', padding=8, spacing=8)
+        h_layout = BoxLayout()
+        txt = Label(text='Выбери экран')
 
 class MyApp(App):                                           # создаем класс-наследник класса App
     def build(self):                                        # переопределяем метод build
