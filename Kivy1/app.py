fdca6d38878eeba1d67bd3be2b31e4067729698c
beverly-csu <80579574+beverly-csu@users.noreply.github.com>
2022-06-14 20:34:16 +0500

    Update app.py

--- Kivy1/app.py
+++ Kivy1/app.py
@@ -3,6 +3,7 @@ from kivy.uix.label import Label                                                
 from kivy.uix.button import Button                                              # подключаем виджет "Кнопка"
 from kivy.uix.boxlayout import BoxLayout                                        # подключаем виджет расположения объектов
 from kivy.uix.screenmanager import ScreenManager, Screen                        # подключаем виджет "Экран"
+from kivy.uix.textinput import TextInput                                        # подключим виджет "Ввод текста"
 
 
 class ScrButton(Button):                                                        # создаем класс-наследник класса Button
@@ -36,12 +37,17 @@ class FirstScreen(Screen):
         super().__init__(**kwargs)
         v_layout = BoxLayout(orientation='vertical', size_hint=(0.5, 0.5), pos_hint={'center_x': 0.5, 'center_y': 0.5})
         btn = Button(text="Выбор: 1", size_hint=(0.5, 1), pos_hint={'left': 0})
+        btn_back = ScrButton(self, direction='up', goal='main', text='Назад', size_hint=(0.5, 1), pos_hint={'right': 1})
+        v_layout.add_widget(btn)
+        v_layout.add_widget(btn_back)
+        self.add_widget(v_layout)
 
 
 class MyApp(App):                                                               # создаем класс-наследник класса App
     def build(self):                                                            # переопределяем метод build
         sm = ScreenManager()                                                    # создаем экземпляр класса ScreenManager()
         sm.add_widget(MainScreen(name='main'))
+        sm.add_widget(FirstScreen(name='first'))
         return sm                                                               # возвращаем наш экземпляр
 
 
