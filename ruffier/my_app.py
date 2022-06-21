6504aa95a9db363f535bd850e6519252de77785e
beverly-csu <80579574+beverly-csu@users.noreply.github.com>
2022-06-21 20:38:25 +0500

    Update my_app.py

--- ruffier/my_app.py
+++ ruffier/my_app.py
@@ -15,3 +15,20 @@ class InstrScreen(Screen):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         instruction = Label(text="Empty")
+
+        lbl_name = Label(text="Введите имя:", halign="right")
+        self.input_name = TextInput(multiline=False)
+
+        lbl_age = Label(text="Введите возраст:", halign="right")
+        self.input_age = TextInput(text="7", multiline=False)
+
+        self.btn = Button(text="Начать", size_hint=(0.3, 0.2), pos_hint={"center_x": 0.5})
+        self.btn.on_press = self.next
+
+        line1 = BoxLayout(size_hint=(0.8, None), height="30sp")
+        line2 = BoxLayout(size_hint=(0.8, None), height='30sp')
+
+
+    def next(self):
+        global name
+        name = self.input_name.text
