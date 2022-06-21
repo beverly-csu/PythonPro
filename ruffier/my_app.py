d2dde809fa0d8338bdf249feab4a0fdaf4abade4
beverly-csu <80579574+beverly-csu@users.noreply.github.com>
2022-06-21 20:43:43 +0500

    Update my_app.py

--- ruffier/my_app.py
+++ ruffier/my_app.py
@@ -27,8 +27,31 @@ class InstrScreen(Screen):
 
         line1 = BoxLayout(size_hint=(0.8, None), height="30sp")
         line2 = BoxLayout(size_hint=(0.8, None), height='30sp')
-
+        
+        line1.add_widget(lbl_name)
+        line1.add_widget(self.input_name)
+        line2.add_widget(lbl_age)
+        line2.add_widget(self.input_age)
+        outer = BoxLayout(orientation="vertical", padding=8, spacing=8)
+        outer.add_widget(instruction)
+        outer.add_widget(line1)
+        outer.add_widget(line2)
+        outer.add_widget(self.btn)
+        self.add_widget(outer)
 
     def next(self):
         global name
         name = self.input_name.text
+        self.manager.current = "pulse1"
+
+class HeartCheck(App):
+    def build(self):
+        sm = ScreenManager()
+        sm.add_widget(InstrScreen(name="instr"))
+        return sm
+
+# первый вариант
+app = HeartCheck()
+app.run()
+# второй вариант
+HeartCheck().run()
