8405cffc360bde62c40ebcf6d618e98962c17b83
beverly-csu <80579574+beverly-csu@users.noreply.github.com>
2022-06-28 19:27:02 +0500

    Update my_app.py

--- ruffier/my_app.py
+++ ruffier/my_app.py
@@ -1,10 +1,12 @@
 #не забудь импортировать необходимые элементы!
+from re import MULTILINE
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
+from instructions import *
 
 age = 7
 name = str()
@@ -14,7 +16,7 @@ p1, p2, p3 = 0, 0, 0
 class InstrScreen(Screen):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        instruction = Label(text="Empty")
+        instruction = Label(text=txt_instruction)
 
         lbl_name = Label(text="Введите имя:", halign="right")
         self.input_name = TextInput(multiline=False)
@@ -44,10 +46,33 @@ class InstrScreen(Screen):
         name = self.input_name.text
         self.manager.current = "pulse1"
 
+class PulseScr(Screen):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        instr = Label(text=txt_test1)
+        line = BoxLayout(size_hint=(0.8, None), height='30sp')
+        lbl_result = Label(text="Введите результат:", halign='right')
+        self.in_result = TextInput(text='0', multiline=False)
+        line.add_widget(lbl_result)
+        line.add_widget(self.in_result)
+        self.btn = Button(text='Продолжить', size_hint=(0.3, 0.2), pos_hint={'center_x': 0.5})
+        self.btn.on_press = self.next
+        outer = BoxLayout(orientation='vertical', padding=8, spacing=8)
+        outer.add_widget(instr)
+        outer.add_widget(line)
+        outer.add_widget(self.btn)
+        self.add_widget(outer)
+    def next(self):
+        global p1
+        p1 = int(self.in_result.text)
+        self.manager.current = 'sits'
+
 class HeartCheck(App):
     def build(self):
         sm = ScreenManager()
         sm.add_widget(InstrScreen(name="instr"))
+        sm.add_widget(PulseScr(name='pulse1'))
         return sm
 
 # первый вариант
