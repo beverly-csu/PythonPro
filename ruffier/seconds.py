40fc95ab15bdef0fc9328ca3393d08da437e86db
beverly-csu <80579574+beverly-csu@users.noreply.github.com>
2022-07-12 19:19:16 +0500

    Update seconds.py

--- ruffier/seconds.py
+++ ruffier/seconds.py
@@ -13,10 +13,18 @@ class Seconds(Label):
         super().__init__(text=my_text)
     
     def restart(self, total, **kwargs):
-        pass
+        self.done = False
+        self.total = total
+        self.current = 0
+        self.text = 'Прошло секунд: ' + str(self.current)
+        self.start()
 
     def start(self):
         Clock.schedule_interval(self.change, 1)
 
     def change(self, dt):
-        pass
+        self.current += 1
+        self.text = 'Прошло секунд: ' + str(self.current)
+        if self.current >= self.total:
+            self.done = True
+            return False
