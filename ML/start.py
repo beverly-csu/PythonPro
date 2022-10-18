cb53a1f8a9a903debafd86ea0bc99975134048cd
beverly-csu <80579574+beverly-csu@users.noreply.github.com>
2022-10-18 21:04:31 +0500

    Update start.py

--- ML/start.py
+++ ML/start.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
 df = pd.read_csv('train.csv')
-df.drop(['id', 'bdate', 'education_form'], axis=1, inplace=True)
 
+df.drop(['id', 'bdate'], axis=1, inplace=True)
 
 def convert_mobile(has_mobile):
     return int(has_mobile)
@@ -40,7 +40,47 @@ def convert_ed_status(ed_status):
         return 9
 
 df['education_status'] = df['education_status'].apply(convert_ed_status)
-df.drop(['langs', 'life_main', 'people_main', 'last_seen', 'occupation_type', 'occupation_name', 'career_start', 'career_end'], axis=1, inplace=True)
+df.drop(['langs', 'life_main', 'people_main', 'last_seen', 'occupation_name'], axis=1, inplace=True)
+
+# 1 & 2 группа
+def make_occupation(occupation_type):
+    if occupation_type == 'work':
+        return 1
+    else:
+        return 0
+
+df['occupation_type'] = df['occupation_type'].apply(make_occupation)
+# 1 & 2 группа
+
+# 1 группа
+def career_start(date):
+    if date == 'False':
+        return 2009
+    else:
+        return int(date)
+
+df['career_start'] = df['career_start'].apply(career_start)
+
+def get_experience(date_start):
+    return 2022 - date_start
+
+df['experience'] = df['career_start'].apply(get_experience)
+
+df.drop(['career_start', 'career_end'], axis=1, inplace=True)
+# 1 группа
+
+# 2 группа
+def get_education_form(ed_form):
+    if ed_form == 'Distance Learning':
+        return 1
+    elif ed_form == 'Part-time':
+        return 2
+    else:
+        return 0
+
+df['education_form'] = df['education_form'].apply(get_education_form)
+# 2 группа
+
 
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
@@ -59,6 +99,6 @@ classifier = KNeighborsClassifier(n_neighbors=5)
 classifier.fit(x_train, y_train)
 
 y_pred = classifier.predict(x_test)
-print('Процент правильно предсказанных исходов:', accuracy_score(y_test, y_pred) * 100)
+print('Процент правильно предсказанных исходов:', round(accuracy_score(y_test, y_pred) * 100, 2) )
 print('Confusion matrix:')
 print(confusion_matrix(y_test, y_pred))
